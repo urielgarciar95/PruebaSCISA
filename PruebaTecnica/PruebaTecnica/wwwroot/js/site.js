3de2bd9a26bfa4a3e2dfa4e5501c7cd0d1ef98ad
Uriel Garcia <urielgarciar95@gmail.com>
2026-08-28 16:57:57 -0600

    Se agrega formulario y se llena combo especies invocando la API de especies

---
+++ PruebaTecnica/PruebaTecnica/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿
+var urlEspecies = "https://pokeapi.co/api/v2/pokemon-species/?offset=0&limit=1025";
+
+$(document).ready(function () {
+    llenarComboEspecies()
+});
+
+function llenarComboEspecies() {
+    $.ajax({
+        url: urlEspecies,
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            /*console.log('Datos recibidos:', response.results.length);*/
+            let comboEspecies = '<option value="">Seleccionar especie...</option>';
+            /*Recorrer arreglo de especies y llenar combo*/
+            for (var x = 0; x < response.results.length; x++) {
+                var item = response.results[x];
+                console.log("nombre: ", item.name);
+                comboEspecies += `<option value="${item.url}">${item.name}</option>`;
+            }
+
+            $("#especies").html(comboEspecies);
+        },
+        error: function (xhr, status, error) {
+            console.error('Ocurrió un error:', error);
+        }
+    });
+}
